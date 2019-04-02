2c22b2b8f9d4033fc083e72573d16b613e70a39b
Darren Young <youngd24@gmail.com>
2019-04-02 13:56:22 -0500

    updated loops

--- scripts/cidr2ip.sh
+++ scripts/cidr2ip.sh
@@ -61,17 +61,14 @@ prefix_to_bit_netmask() {
     shift=$(( 32 - prefix ))
 
     bitmask=""
-    for (( i=0; i < 32; i++ ));
-    do
+    for (( i=0; i < 32; i++ )); do
         num=0
-        if [ $i -lt $prefix ];
-        then
+        if [ $i -lt $prefix ]; then
             num=1
         fi
 
         space=
-        if [ $(( i % 8 )) -eq 0 ];
-        then
+        if [ $(( i % 8 )) -eq 0 ]; then
             space=" "
         fi
 
@@ -92,8 +89,7 @@ prefix_to_bit_netmask() {
 bit_netmask_to_wildcard_netmask() {
     bitmask=$1
     wildcard_mask=
-    for octet in $bitmask;
-    do
+    for octet in $bitmask; do
         wildcard_mask="${wildcard_mask} $(( 255 - 2#$octet ))"
     done
     echo $wildcard_mask
@@ -112,14 +108,11 @@ check_net_boundary() {
     net=$1
     wildcard_mask=$2
     is_correct=1
-    for (( i = 1; i <= 4; i++ ));
-    do
+    for (( i = 1; i <= 4; i++ )); do
         net_octet=$(echo $net | cut -d '.' -f $i)
         mask_octet=$(echo $wildcard_mask | cut -d ' ' -f $i)
-        if [ $mask_octet -gt 0 ];
-        then
-            if [ $(( $net_octet&$mask_octet )) -ne 0 ];
-            then
+        if [ $mask_octet -gt 0 ]; then
+            if [ $(( $net_octet&$mask_octet )) -ne 0 ]; then
                 is_correct=0
             fi
         fi
@@ -136,15 +129,13 @@ getopts "fibh" force
 
 shift $((OPTIND-1))
 
-if [ $force = 'h' ];
-then
+if [ $force = 'h' ]; then
     echo ""
     echo -e "THIS SCRIPT WILL EXPAND A CIDR ADDRESS.\n\nSYNOPSIS\n  ./cidr-to-ip.sh [OPTION(only one)] [STRING/FILENAME]\nDESCRIPTION\n -h  Displays this help screen\n -f  Forces a check for network boundary when given a STRING(s)\n    -i  Will read from an Input file (no network boundary check)\n  -b  Will do the same as –i but with network boundary check\n\nEXAMPLES\n    ./cidr-to-ip.sh  192.168.0.1/24\n   ./cidr-to-ip.sh  192.168.0.1/24 10.10.0.0/28\n  ./cidr-to-ip.sh  -f 192.168.0.0/16\n    ./cidr-to-ip.sh  -i inputfile.txt\n ./cidr-to-ip.sh  -b inputfile.txt\n"
     exit
 fi
 
-if [ $force = 'i' ] || [ $force = 'b' ];
-then
+if [ $force = 'i' ] || [ $force = 'b' ]; then
     old_IPS=$IPS
     IPS=$'\n'
     lines=($(cat $1)) # array
@@ -153,8 +144,7 @@ else
             lines=$@
 fi
 
-for ip in ${lines[@]};
-do
+for ip in ${lines[@]}; do
     net=$(echo $ip | cut -d '/' -f 1)
     prefix=$(echo $ip | cut -d '/' -f 2)
     do_processing=1
@@ -167,10 +157,10 @@ do
     if [ $force = 'f' ] && \
        [ $is_net_boundary -ne 1 ] || \
        [ $force = 'b' ]&& \
-       [ $is_net_boundary -ne 1 ] ;
-    then
+       [ $is_net_boundary -ne 1 ] ; then
         read -p "Not a network boundary! Continue anyway (y/N)? " -n 1 -r
         echo    ## move to a new line
+
         if [[ $REPLY =~ ^[Yy]$ ]];
         then
             do_processing=1
@@ -179,14 +169,12 @@ do
         fi
     fi  
 
-    if [ $do_processing -eq 1 ];
-    then
+    if [ $do_processing -eq 1 ]; then
         str=
         for (( i = 1; i <= 4; i++ )); do
             range=$(echo $net | cut -d '.' -f $i)
             mask_octet=$(echo $wildcard_mask | cut -d ' ' -f $i)
-            if [ $mask_octet -gt 0 ];
-            then
+            if [ $mask_octet -gt 0 ]; then
                 range="{$range..$(( $range | $mask_octet ))}"
             fi
             str="${str} $range"
